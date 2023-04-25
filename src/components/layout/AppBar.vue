76a7770872819199b1bca673f36d5a0b80d22c1b
Eduardo Moraes Goncalves <eduardogoncalves18@gmail.com>
2023-04-25 17:11:14 -0300

    Criação da barra de navegação e formulário de cadastro e configuração de app

---
+++ src/components/layout/AppBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-layout>
+    <v-navigation-drawer expand-on-hover rail rounded="lg">
+      <v-list>
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+          title="Sandra Adams"
+          subtitle="sandra_a88@gmail.com"
+        ></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+        <config-dialog></config-dialog>
+        <v-list-item
+          prepend-icon="mdi-account-multiple"
+          title="Shared with me"
+          value="shared"
+        ></v-list-item>
+        <v-list-item prepend-icon="mdi-star" title="Home" @click.prevent="homeView"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main style="height: 300px"></v-main>
+  </v-layout>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import ConfigDialog from '@/components/dialog/ConfigAppDialog.vue';
+
+const router = useRouter();
+
+const homeView = () => {
+  router.push({ name: 'home' });
+};
+
+const configApp = () => {
+  router.push({ name: 'home' });
+};
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,103 @@
+<template>
+  <v-sheet class="bg-deep-purple home">
+    <v-card class="mx-auto px-6 py-8" min-width="428">
+      <v-form v-model="form">
+        <v-text-field
+          v-model="email"
+          :readonly="loading"
+          class="mb-2"
+          clearable
+          label="Email"
+          v-model.trim="email"
+          :rules="[rules.required, rules.email]"
+          outlined
+        ></v-text-field>
+
+        <v-text-field
+          v-model="password"
+          :readonly="loading"
+          clearable
+          label="Password"
+          placeholder="Enter your password"
+          v-model.trim="password"
+          :rules="[rules.required, rules.password]"
+          :type="showPassword ? 'text' : 'password'"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="() => (showPassword = !showPassword)"
+          outlined
+        ></v-text-field>
+
+        <br />
+
+        <v-checkbox v-model="rememberMe" label="Lembrar-me" hide-details></v-checkbox>
+
+        <v-btn
+          :disabled="!form"
+          :loading="loading"
+          block
+          color="success"
+          size="large"
+          type="submit"
+          variant="elevated"
+          @click.prevent="onSubmit"
+        >
+          Sign In
+        </v-btn>
+      </v-form>
+
+      <br />
+
+      <v-btn color="red" size="large" type="submit" variant="elevated" @click.prevent="loginView">
+        Login
+      </v-btn>
+    </v-card>
+  </v-sheet>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const form = ref(false);
+const email = ref('');
+const password = ref('');
+const rememberMe = ref(false);
+const showPassword = ref(false);
+const loading = ref(false);
+
+const rules = {
+  required: (value: string) => !!value || 'Campo obrigatório',
+  email: (value: string) =>
+    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/.test(value) || 'E-mail inválido',
+  password: (value: string) =>
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(value) ||
+    'Senha deve ter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula e um número.'
+};
+const onSubmit = () => {
+  console.log(email.value);
+
+  // Submit to API
+  if (!form.value) return;
+
+  loading.value = true;
+
+  setTimeout(() => (loading.value = false), 2000);
+};
+
+const router = useRouter();
+
+const loginView = () => {
+  router.push({ name: 'login' });
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    min-width: 100vw;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
